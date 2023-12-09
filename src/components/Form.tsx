0a4eef30913d99eb4be7b195ba6734aae42c1047
marrcelp <marcel.piaszczyk@gmail.com>
2023-12-09 02:57:46 +0100

    fixed and styled drink generator

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
 import {useState, useEffect } from 'react';
 import {getDrinks, DrinkRecipe } from "./api/Recipes.tsx";
+import generatorimg from '../../src/images/generator-img.png';
 
 function getRecipeIngredients(recipe: DrinkRecipe): string[] {
     const ingredients: string[] = [];
@@ -95,130 +96,135 @@ const Form: React.FC<{ isVisible: boolean }> = ({ isVisible }) => {
     return (
         <section style={{ display: isVisible ? 'block' : 'none' }} id='generator' className='generator'>
             <h1 className='class-title'>CHOOSE YOUR INGREDIENTS, WE WILL PREPARE A RECIPE FOR YOU!</h1>
-            <div className='form-group'>
-                <label>
-                    <input
-                        type='radio'
-                        name='ingredientType'
-                        value='gin'
-                        // checked={selectedIngredientType === 'gin'}
-                        defaultChecked={true}
-                        onChange={() => {
+            <div className='generator-container'>
+                <div className='form-container'>
+                    <div className='form-group'>
+                        <label>
+                            <input className='input-radio'
+                                type='radio'
+                                name='ingredientType'
+                                value='gin'
+                                // checked={selectedIngredientType === 'gin'}
+                                defaultChecked={true}
+                                onChange={() => {
 
-                            setSelectedIngredientType('gin')
-                            // setSelectedIngredients(prevIngredients => [...prevIngredients, 'Gin'])
-                        }}
-                    />
-                    Gin
-                </label>
-                <label>
-                    <input
-                        type='radio'
-                        name='ingredientType'
-                        value='vodka'
-                        // checked={selectedIngredientType === 'vodka'}
-                        // defaultChecked={false}
-                        onChange={() => {
-                            setSelectedIngredientType('vodka')
-                            // setSelectedIngredients(prevIngredients => [...prevIngredients, 'Vodka'])
-                        }}
-                    />
-                    Vodka
-                </label>
-                <label>
-                    <input
-                        type='radio'
-                        name='ingredientType'
-                        value='tequila'
-                        // checked={selectedIngredientType === 'vodka'}
-                        // defaultChecked={false}
-                        onChange={() => {
-                            setSelectedIngredientType('tequila')
-                            // setSelectedIngredients(prevIngredients => [...prevIngredients, 'Vodka'])
-                        }}
-                    />
-                    Tequila
-                </label>
-            </div>
-            <form onSubmit={(e) => handleIngredients(e)}>
-                <div className='form-group'>
-                    <input
-                        type='text'
-                        className='form-control'
-                        name='ingredient'
-                        placeholder='ex. Tonic Water'
-                        value={ingredient}
-                        onChange={(e) => setIngredient(e.target.value)}
-                    />
+                                    setSelectedIngredientType('gin')
+                                    // setSelectedIngredients(prevIngredients => [...prevIngredients, 'Gin'])
+                                }}
+                            />
+                            &nbsp;&nbsp;&nbsp;Gin
+                        </label>
+                        <label>
+                            <input className='input-radio'
+                                type='radio'
+                                name='ingredientType'
+                                value='vodka'
+                                // checked={selectedIngredientType === 'vodka'}
+                                // defaultChecked={false}
+                                onChange={() => {
+                                    setSelectedIngredientType('vodka')
+                                    // setSelectedIngredients(prevIngredients => [...prevIngredients, 'Vodka'])
+                                }}
+                            />
+                            &nbsp;Vodka
+                        </label>
+                        <label>
+                            <input className='input-radio'
+                                type='radio'
+                                name='ingredientType'
+                                value='tequila'
+                                // checked={selectedIngredientType === 'vodka'}
+                                // defaultChecked={false}
+                                onChange={() => {
+                                    setSelectedIngredientType('tequila')
+                                    // setSelectedIngredients(prevIngredients => [...prevIngredients, 'Vodka'])
+                                }}
+                            />
+                            Tequila
+                        </label>
+                    </div>
+                    <form onSubmit={(e) => handleIngredients(e)}>
+                        <div className='form-group'>
+                            <input
+                                type='text'
+                                className='form-control'
+                                name='ingredient'
+                                placeholder='ex. Tonic Water'
+                                value={ingredient}
+                                onChange={(e) => setIngredient(e.target.value)}
+                            />
+                        </div>
+                        <button type='submit' className='btn'>
+                            ADD INGREDIENT
+                        </button>
+                    </form>
+
+
+                    <div className='ingredients'>
+                        <h2>Selected ingredients:</h2>
+                        <ul>
+                            {selectedIngredients.map((ingredient, index) => (
+                                    <li key={index}>
+                                        {ingredient}
+                                        <button className='btn-delete' onClick={() => handleRemove(ingredient)}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;X</button>
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>
+
+                    <div className='recipes'>
+                        <h2>Your recipes with selected ingredients:</h2>
+                        <ul>
+
+                            {filteredRecipes.map((recipe, index) => (
+                                <li key={index}>
+                                    <div onClick={() => handleRecipeClick(recipe)}>
+                                        {recipe.strDrink}
+                                    </div>
+                                    {selectedRecipe && selectedRecipe.idDrink === recipe.idDrink && isExpanded && (
+                                        <div className='drink_details'>
+                                            <img src={selectedRecipe.strDrinkThumb}
+                                                 style={{width: '150px', height: '150px', border: '2px solid white', borderRadius: '20px'}}
+                                                 alt='Drink image'
+                                            />
+                                            <h3>Instruction:</h3>
+                                            <p>{selectedRecipe.strInstructions}</p>
+                                            <h3>Ingredients:</h3>
+                                            <ul>
+                                                {(() => {
+
+                                                    const ingredients = [];
+                                                    for (let i = 1; i <= 15; i++) {
+                                                        const ingredientKey = `strIngredient${i}` as keyof DrinkRecipe;
+                                                        const measureKey = `strMeasure${i}` as keyof DrinkRecipe;
+                                                        const measure: any = selectedRecipe[measureKey];
+                                                        const ingredient: any = selectedRecipe[ingredientKey];
+                                                        const selectedIngredientstoLower = selectedIngredients.map(element => element.toLowerCase());
+                                                        if (ingredient) {
+                                                            ingredients.push(<li style={{ color: selectedIngredientstoLower.includes(
+                                                                ingredient.toLowerCase()) ||
+                                                                (ingredient.toLowerCase() === 'gin' ||
+                                                                    ingredient.toLowerCase() === 'vodka'||
+                                                                    ingredient.toLowerCase() === 'tequila') ?
+                                                                    '#008e00' : '#d13030' }} key={i}>{ingredient} - {measure}</li>);
+                                                        }
+
+                                                    }
+                                                    return ingredients;
+                                                })()}
+                                            </ul>
+                                            <MissingIngredients
+                                                selectedIngredients={selectedIngredients}
+                                                recipeIngredients={getRecipeIngredients(recipe)}
+                                            />
+                                        </div>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
-                <button type='submit' className='btn'>
-                    Add ingredient
-                </button>
-            </form>
-
-
-            <div>
-                <h2>Selected ingredients:</h2>
-                <ul>
-                    {selectedIngredients.map((ingredient, index) => (
-                            <li key={index}>
-                                {ingredient}
-                                <button onClick={() => handleRemove(ingredient)}>X</button>
-                            </li>
-                        ))}
-                </ul>
-            </div>
-
-            <div className='selected_ingredients'>
-                <h2>Your recipes with selected ingredients:</h2>
-                <ul>
-
-                    {filteredRecipes.map((recipe, index) => (
-                        <li key={index}>
-                            <div onClick={() => handleRecipeClick(recipe)}>
-                                {recipe.strDrink}
-                            </div>
-                            {selectedRecipe && selectedRecipe.idDrink === recipe.idDrink && isExpanded && (
-                                <div className='drink_details'>
-                                    <img src={selectedRecipe.strDrinkThumb}
-                                         style={{width: '150px', height: '150px', border: '2px solid black', borderRadius: '20px'}}
-                                         alt='Drink image'
-                                    />
-                                    <h3>Instruction:</h3>
-                                    <p>{selectedRecipe.strInstructions}</p>
-                                    <h3>Ingredients:</h3>
-                                    <ul>
-                                        {(() => {
-
-                                            const ingredients = [];
-                                            for (let i = 1; i <= 15; i++) {
-                                                const ingredientKey = `strIngredient${i}` as keyof DrinkRecipe;
-                                                const measureKey = `strMeasure${i}` as keyof DrinkRecipe;
-                                                const measure: any = selectedRecipe[measureKey];
-                                                const ingredient: any = selectedRecipe[ingredientKey];
-                                                const selectedIngredientstoLower = selectedIngredients.map(element => element.toLowerCase());
-                                                if (ingredient) {
-                                                    ingredients.push(<li style={{ color: selectedIngredientstoLower.includes(
-                                                        ingredient.toLowerCase()) ||
-                                                        (ingredient.toLowerCase() === 'gin' ||
-                                                            ingredient.toLowerCase() === 'vodka'||
-                                                            ingredient.toLowerCase() === 'tequila') ?
-                                                            'green' : 'red' }} key={i}>{ingredient} - {measure}</li>);
-                                                }
-
-                                            }
-                                            return ingredients;
-                                        })()}
-                                    </ul>
-                                    <MissingIngredients
-                                        selectedIngredients={selectedIngredients}
-                                        recipeIngredients={getRecipeIngredients(recipe)}
-                                    />
-                                </div>
-                            )}
-                        </li>
-                    ))}
-                </ul>
+                <img src={generatorimg} className='generator_image'/>
             </div>
 
 
